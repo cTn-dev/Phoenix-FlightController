63250b13435bf723db974b361925a9c518ae6e4e
X-warrior <matheusbrat@gmail.com>
2013-02-01 12:01:45 -0200

    Adding support to ACCEL on dataStorage, fixing some indentation problems on ACCEL as well. Changed dataStorage include position so it can be accessed on other included files.

--- libraries/P_Accelerometer/ADXL345.h
+++ libraries/P_Accelerometer/ADXL345.h
@@ -21,92 +21,97 @@ class ADXL345 {
         
             accelSamples = 0;
         };
-    
-    void initialize() {
-        sensors.i2c_write8(ADXL345_ADDRESS, 0x2D, 1 << 3);     // set device to *measure*
-        sensors.i2c_write8(ADXL345_ADDRESS, 0x31, 0x09);       // set full range and +/- 4G
-        sensors.i2c_write8(ADXL345_ADDRESS, 0x2C, 8 + 2 + 1);  // 200hz sampling
-    };
-    
-    // ~1280ms (only runs when requested)
-    void calibrate_accel() {
-        uint8_t i, count = 128;
-        int32_t xSum = 0, ySum = 0, zSum = 0;
+        void initialize(int bias0, int bias1, int bias2) {
+            accel_bias[XAXIS] = bias0;
+            accel_bias[YAXIS] = bias1;
+            accel_bias[ZAXIS] = bias2;
+            initialize();
+        };
+        void initialize() {
+            sensors.i2c_write8(ADXL345_ADDRESS, 0x2D, 1 << 3);     // set device to *measure*
+            sensors.i2c_write8(ADXL345_ADDRESS, 0x31, 0x09);       // set full range and +/- 4G
+            sensors.i2c_write8(ADXL345_ADDRESS, 0x2C, 8 + 2 + 1);  // 200hz sampling
+        };
 
-        for(i = 0; i < count; i++) {
+        // ~1280ms (only runs when requested)
+        void calibrate_accel() {
+            uint8_t i, count = 128;
+            int32_t xSum = 0, ySum = 0, zSum = 0;
+
+            for(i = 0; i < count; i++) {
+                readAccelRaw();
+                xSum += accelRaw[XAXIS];
+                ySum += accelRaw[YAXIS];
+                zSum += accelRaw[ZAXIS];
+                delay(10);
+            }
+
+            accel_bias[XAXIS] = xSum / count;
+            accel_bias[YAXIS] = ySum / count;
+            accel_bias[ZAXIS] = zSum / count;
+    
+            // Code will stop here, printing out the calibration data in serial console.
+            // Re-Calibrating each loop.
+            // Offsets to the values returned on a flat surface are meant to be either hardcoded during initialization
+            // or stored inside eeprom.
+            while (1) {
+                Serial.print(accel_bias[XAXIS]);
+                Serial.write('\t');
+                Serial.print(accel_bias[YAXIS]);
+                Serial.write('\t');
+                Serial.print(accel_bias[ZAXIS]);
+                Serial.write('\t');
+                Serial.println();
+
+                delay(5000);
+                calibrate_accel();
+            }
+        };
+
+        void readAccelRaw() {
+            Wire.beginTransmission(ADXL345_ADDRESS);
+            Wire.write(0x32);
+            Wire.endTransmission();
+
+            Wire.requestFrom(ADXL345_ADDRESS, 6);
+
+            accelRaw[XAXIS] = Wire.read() | (Wire.read() << 8);
+            accelRaw[YAXIS] = Wire.read() | (Wire.read() << 8);
+            accelRaw[ZAXIS] = Wire.read() | (Wire.read() << 8);
+        };
+
+        void readAccelSum() {
             readAccelRaw();
-            xSum += accelRaw[XAXIS];
-            ySum += accelRaw[YAXIS];
-            zSum += accelRaw[ZAXIS];
-            delay(10);
-        }
-        
-        accel_bias[XAXIS] = xSum / count;
-        accel_bias[YAXIS] = ySum / count;
-        accel_bias[ZAXIS] = zSum / count;
-
-        // Code will stop here, printing out the calibration data in serial console.
-        // Re-Calibrating each loop.
-        // Offsets to the values returned on a flat surface are meant to be either hardcoded during initialization
-        // or stored inside eeprom.
-        while (1) {
-            Serial.print(accel_bias[XAXIS]);
-            Serial.write('\t');
-            Serial.print(accel_bias[YAXIS]);
-            Serial.write('\t');
-            Serial.print(accel_bias[ZAXIS]);
-            Serial.write('\t');
-            Serial.println();
             
-            delay(5000);
-            calibrate_accel();
-        }
-    };    
+            accelSum[XAXIS] += accelRaw[XAXIS];
+            accelSum[YAXIS] += accelRaw[YAXIS];
+            accelSum[ZAXIS] += accelRaw[ZAXIS];
     
-    void readAccelRaw() {
-        Wire.beginTransmission(ADXL345_ADDRESS);
-        Wire.write(0x32);
-        Wire.endTransmission();
-        
-        Wire.requestFrom(ADXL345_ADDRESS, 6);   
- 
-        accelRaw[XAXIS] = Wire.read() | (Wire.read() << 8);
-        accelRaw[YAXIS] = Wire.read() | (Wire.read() << 8); 
-        accelRaw[ZAXIS] = Wire.read() | (Wire.read() << 8);    
-    };
+            accelSamples++;
+        };
     
-    void readAccelSum() {
-        readAccelRaw();
-        
-        accelSum[XAXIS] += accelRaw[XAXIS];
-        accelSum[YAXIS] += accelRaw[YAXIS];
-        accelSum[ZAXIS] += accelRaw[ZAXIS];  
+        void evaluateAccel() {
+            // Calculate average
+            accel[XAXIS] = accelSum[XAXIS] / accelSamples;
+            accel[YAXIS] = accelSum[YAXIS] / accelSamples;
+            accel[ZAXIS] = accelSum[ZAXIS] / accelSamples;
 
-        accelSamples++;
-    };    
+            // Apply offsets
+            accel[XAXIS] += accel_bias[XAXIS];
+            accel[YAXIS] += accel_bias[YAXIS];
+            accel[ZAXIS] += accel_bias[ZAXIS];
 
-    void evaluateAccel() {
-        // Calculate average
-        accel[XAXIS] = accelSum[XAXIS] / accelSamples;
-        accel[YAXIS] = accelSum[YAXIS] / accelSamples;
-        accel[ZAXIS] = accelSum[ZAXIS] / accelSamples;  
-        
-        // Apply offsets
-        accel[XAXIS] += accel_bias[XAXIS];
-        accel[YAXIS] += accel_bias[YAXIS];
-        accel[ZAXIS] += accel_bias[ZAXIS];
-        
-        // Apply correct scaling (at this point accel reprensents +- 1g = 9.81 m/s^2)
-        accel[XAXIS] *= accelScaleFactor;
-        accel[YAXIS] *= accelScaleFactor;
-        accel[ZAXIS] *= accelScaleFactor;
-        
-        // Reset SUM variables
-        accelSum[XAXIS] = 0;
-        accelSum[YAXIS] = 0;
-        accelSum[ZAXIS] = 0;
-        accelSamples = 0;
-    };
+            // Apply correct scaling (at this point accel reprensents +- 1g = 9.81 m/s^2)
+            accel[XAXIS] *= accelScaleFactor;
+            accel[YAXIS] *= accelScaleFactor;
+            accel[ZAXIS] *= accelScaleFactor;
+
+            // Reset SUM variables
+            accelSum[XAXIS] = 0;
+            accelSum[YAXIS] = 0;
+            accelSum[ZAXIS] = 0;
+            accelSamples = 0;
+        };
     
     private:
         int16_t accel_bias[3];
@@ -121,7 +126,7 @@ class ADXL345 {
 ADXL345 adxl;
 
 void SensorArray::initializeAccel() {
-    adxl.initialize();
+    adxl.initialize(CONFIG.data.ACCEL_BIAS[0], CONFIG.data.ACCEL_BIAS[1], CONFIG.data.ACCEL_BIAS[2]);
     
     // Only runs when requested
     // adxl.calibrate_accel();
--- libraries/P_Accelerometer/ADXL345_AQ_v21.h
+++ libraries/P_Accelerometer/ADXL345_AQ_v21.h
@@ -21,92 +21,98 @@ class ADXL345 {
         
             accelSamples = 0;
         };
+        void initialize(int bias0, int bias1, int bias2) {
+            accel_bias[XAXIS] = bias0;
+            accel_bias[YAXIS] = bias1;
+            accel_bias[ZAXIS] = bias2;
+            initialize();
+        };
     
-    void initialize() {
-        sensors.i2c_write8(ADXL345_ADDRESS, 0x2D, 1 << 3);     // set device to *measure*
-        sensors.i2c_write8(ADXL345_ADDRESS, 0x31, 0x09);       // set full range and +/- 4G
-        sensors.i2c_write8(ADXL345_ADDRESS, 0x2C, 8 + 2 + 1);  // 200hz sampling
-    };
-    
-    // ~1280ms (only runs when requested)
-    void calibrate_accel() {
-        uint8_t i, count = 128;
-        int32_t xSum = 0, ySum = 0, zSum = 0;
-
-        for(i = 0; i < count; i++) {
-            readAccelRaw();
-            xSum += accelRaw[XAXIS];
-            ySum += accelRaw[YAXIS];
-            zSum += accelRaw[ZAXIS];
-            delay(10);
-        }
+        void initialize() {
+            sensors.i2c_write8(ADXL345_ADDRESS, 0x2D, 1 << 3);     // set device to *measure*
+            sensors.i2c_write8(ADXL345_ADDRESS, 0x31, 0x09);       // set full range and +/- 4G
+            sensors.i2c_write8(ADXL345_ADDRESS, 0x2C, 8 + 2 + 1);  // 200hz sampling
+        };
         
-        accel_bias[XAXIS] = xSum / count;
-        accel_bias[YAXIS] = ySum / count;
-        accel_bias[ZAXIS] = zSum / count;
+        // ~1280ms (only runs when requested)
+        void calibrate_accel() {
+            uint8_t i, count = 128;
+            int32_t xSum = 0, ySum = 0, zSum = 0;
 
-        // Code will stop here, printing out the calibration data in serial console.
-        // Re-Calibrating each loop.
-        // Offsets to the values returned on a flat surface are meant to be either hardcoded during initialization
-        // or stored inside eeprom.
-        while (1) {
-            Serial.print(accel_bias[XAXIS]);
-            Serial.write('\t');
-            Serial.print(accel_bias[YAXIS]);
-            Serial.write('\t');
-            Serial.print(accel_bias[ZAXIS]);
-            Serial.write('\t');
-            Serial.println();
+            for(i = 0; i < count; i++) {
+                readAccelRaw();
+                xSum += accelRaw[XAXIS];
+                ySum += accelRaw[YAXIS];
+                zSum += accelRaw[ZAXIS];
+                delay(10);
+            }
             
-            delay(5000);
-            calibrate_accel();
-        }
-    };    
+            accel_bias[XAXIS] = xSum / count;
+            accel_bias[YAXIS] = ySum / count;
+            accel_bias[ZAXIS] = zSum / count;
     
-    void readAccelRaw() {
-        Wire.beginTransmission(ADXL345_ADDRESS);
-        Wire.write(0x32);
-        Wire.endTransmission();
+            // Code will stop here, printing out the calibration data in serial console.
+            // Re-Calibrating each loop.
+            // Offsets to the values returned on a flat surface are meant to be either hardcoded during initialization
+            // or stored inside eeprom.
+            while (1) {
+                Serial.print(accel_bias[XAXIS]);
+                Serial.write('\t');
+                Serial.print(accel_bias[YAXIS]);
+                Serial.write('\t');
+                Serial.print(accel_bias[ZAXIS]);
+                Serial.write('\t');
+                Serial.println();
+
+                delay(5000);
+                calibrate_accel();
+            }
+        };
+
+        void readAccelRaw() {
+            Wire.beginTransmission(ADXL345_ADDRESS);
+            Wire.write(0x32);
+            Wire.endTransmission();
+
+            Wire.requestFrom(ADXL345_ADDRESS, 6);
+
+            accelRaw[YAXIS] = Wire.read() | (Wire.read() << 8);
+            accelRaw[XAXIS] = Wire.read() | (Wire.read() << 8);
+            accelRaw[ZAXIS] = Wire.read() | (Wire.read() << 8);
+        };
         
-        Wire.requestFrom(ADXL345_ADDRESS, 6);   
- 
-        accelRaw[YAXIS] = Wire.read() | (Wire.read() << 8);
-        accelRaw[XAXIS] = Wire.read() | (Wire.read() << 8); 
-        accelRaw[ZAXIS] = Wire.read() | (Wire.read() << 8);    
-    };
+        void readAccelSum() {
+            readAccelRaw();
+
+            accelSum[XAXIS] += accelRaw[XAXIS];
+            accelSum[YAXIS] += accelRaw[YAXIS];
+            accelSum[ZAXIS] += accelRaw[ZAXIS];
+
+            accelSamples++;
+        };
     
-    void readAccelSum() {
-        readAccelRaw();
-        
-        accelSum[XAXIS] += accelRaw[XAXIS];
-        accelSum[YAXIS] += accelRaw[YAXIS];
-        accelSum[ZAXIS] += accelRaw[ZAXIS];  
+        void evaluateAccel() {
+            // Calculate average
+            accel[XAXIS] = accelSum[XAXIS] / accelSamples;
+            accel[YAXIS] = accelSum[YAXIS] / accelSamples;
+            accel[ZAXIS] = accelSum[ZAXIS] / accelSamples;
 
-        accelSamples++;
-    };    
+            // Apply offsets
+            accel[XAXIS] += accel_bias[XAXIS];
+            accel[YAXIS] += accel_bias[YAXIS];
+            accel[ZAXIS] += accel_bias[ZAXIS];
 
-    void evaluateAccel() {
-        // Calculate average
-        accel[XAXIS] = accelSum[XAXIS] / accelSamples;
-        accel[YAXIS] = accelSum[YAXIS] / accelSamples;
-        accel[ZAXIS] = accelSum[ZAXIS] / accelSamples;  
-        
-        // Apply offsets
-        accel[XAXIS] += accel_bias[XAXIS];
-        accel[YAXIS] += accel_bias[YAXIS];
-        accel[ZAXIS] += accel_bias[ZAXIS];
-        
-        // Apply correct scaling (at this point accel reprensents +- 1g = 9.81 m/s^2)
-        accel[XAXIS] *= accelScaleFactor;
-        accel[YAXIS] *= accelScaleFactor;
-        accel[ZAXIS] *= accelScaleFactor;
-        
-        // Reset SUM variables
-        accelSum[XAXIS] = 0;
-        accelSum[YAXIS] = 0;
-        accelSum[ZAXIS] = 0;
-        accelSamples = 0;
-    };
+            // Apply correct scaling (at this point accel reprensents +- 1g = 9.81 m/s^2)
+            accel[XAXIS] *= accelScaleFactor;
+            accel[YAXIS] *= accelScaleFactor;
+            accel[ZAXIS] *= accelScaleFactor;
+
+            // Reset SUM variables
+            accelSum[XAXIS] = 0;
+            accelSum[YAXIS] = 0;
+            accelSum[ZAXIS] = 0;
+            accelSamples = 0;
+        };
     
     private:
         int16_t accel_bias[3];
@@ -121,7 +127,7 @@ class ADXL345 {
 ADXL345 adxl;
 
 void SensorArray::initializeAccel() {
-    adxl.initialize();
+    adxl.initialize(CONFIG.data.ACCEL_BIAS[0], CONFIG.data.ACCEL_BIAS[1], CONFIG.data.ACCEL_BIAS[2]);
     
     // Only runs when requested
     // adxl.calibrate_accel();
