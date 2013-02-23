d06cbb620a205ba7cd1b6ed04df6fa83538f6d17
cTn <ctndesigner@gmail.com>
2013-02-23 15:20:26 +0100

    part 2/3 of the channel assignment commit

--- extras/Configurator/tabs/channel_assigner.js
+++ extras/Configurator/tabs/channel_assigner.js
@@ -1,2 +1,40 @@
 function tab_initialize_channel_assigner() {
+    var i = 0;
+    
+    $('#content table.channel_assigner input').each(function() {
+        $(this).val(eepromConfig.CHANNEL_ASSIGNMENT[i++]);
+    });
+    
+    // UI hooks
+    $('#content table.channel_assigner a.update').click(function() {
+        var parent = $(this).parent().parent();
+        
+        // - 1 because .index() starts at 1 not 0
+        eepromConfig.CHANNEL_ASSIGNMENT[parent.index() - 1] = parseInt($('input', parent).val());
+        
+        var eepromConfigBytes = new ArrayBuffer(eepromConfigSize);
+        var view = new DataView(eepromConfigBytes, 0);
+        view.setUNION(eepromConfig); 
+
+        var bufferOut = new ArrayBuffer(5);
+        var bufView = new Uint8Array(bufferOut);
+        
+        // sync char 1, sync char 2, command, payload length MSB, payload length LSB, payload
+        bufView[0] = 0xB5; // sync char 1
+        bufView[1] = 0x62; // sync char 2
+        bufView[2] = 0x02; // command
+        bufView[3] = highByte(eepromConfigSize); // payload length MSB
+        bufView[4] = lowByte(eepromConfigSize); // payload length LSB   
+        
+        chrome.serial.write(connectionId, bufferOut, function(writeInfo) {});
+        
+        // payload
+        chrome.serial.write(connectionId, eepromConfigBytes, function(writeInfo) {
+            if (writeInfo.bytesWritten > 0) {
+                console.log("Wrote: " + writeInfo.bytesWritten + " bytes");
+                
+                command_log('Sending Configuration UNION to Flight Controller ...');
+            }    
+        });       
+    });
 }
