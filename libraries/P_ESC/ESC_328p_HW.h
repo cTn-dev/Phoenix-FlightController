8c50ba90c410fe4f143ccc2b3411f27005a257bd
cTn <ctndesigner@gmail.com>
2013-03-04 03:08:15 +0100

    optimizing hardcoded stuff in ESC calibration

--- libraries/P_ESC/ESC_328p_HW.h
+++ libraries/P_ESC/ESC_328p_HW.h
@@ -93,79 +93,19 @@ void initializeESC() {
         // Calibration sequence requested
         
         // Signal range TOP maximum
-        #if MOTORS == 3
-            MotorOut[0] = 2000;
-            MotorOut[1] = 2000;
-            MotorOut[2] = 2000;
-            
-            updateMotors();
-        #elif MOTORS == 4
-            MotorOut[0] = 2000;
-            MotorOut[1] = 2000;
-            MotorOut[2] = 2000;
-            MotorOut[3] = 2000;
-            
-            updateMotors();
-        #elif MOTORS == 6
-            MotorOut[0] = 2000;
-            MotorOut[1] = 2000;
-            MotorOut[2] = 2000;
-            MotorOut[3] = 2000;
-            MotorOut[4] = 2000;
-            MotorOut[5] = 2000;    
-
-            updateMotors();
-        #elif MOTORS == 8
-            MotorOut[0] = 2000;
-            MotorOut[1] = 2000;
-            MotorOut[2] = 2000;
-            MotorOut[3] = 2000;
-            MotorOut[4] = 2000;
-            MotorOut[5] = 2000;  
-            MotorOut[6] = 2000;
-            MotorOut[7] = 2000;
-            
-            updateMotors();
-        #endif
+        for (uint8_t motor = 0; motor <= MOTORS; motor++) {
+            MotorOut[motor] = 2000;
+        }
+        updateMotors();
         
         // Wait for all ESCs to acknowledge (1 beep)
         delay(5000);
         
         // Signal range BOTTOM minimum
-        #if MOTORS == 3
-            MotorOut[0] = 1000;
-            MotorOut[1] = 1000;
-            MotorOut[2] = 1000;
-            
-            updateMotors();
-        #elif MOTORS == 4
-            MotorOut[0] = 1000;
-            MotorOut[1] = 1000;
-            MotorOut[2] = 1000;
-            MotorOut[3] = 1000;
-            
-            updateMotors();
-        #elif MOTORS == 6
-            MotorOut[0] = 1000;
-            MotorOut[1] = 1000;
-            MotorOut[2] = 1000;
-            MotorOut[3] = 1000;
-            MotorOut[4] = 1000;
-            MotorOut[5] = 1000;    
-
-            updateMotors();
-        #elif MOTORS == 8
-            MotorOut[0] = 1000;
-            MotorOut[1] = 1000;
-            MotorOut[2] = 1000;
-            MotorOut[3] = 1000;
-            MotorOut[4] = 1000;
-            MotorOut[5] = 1000;  
-            MotorOut[6] = 1000;
-            MotorOut[7] = 1000;
-            
-            updateMotors();
-        #endif      
+        for (uint8_t motor = 0; motor <= MOTORS; motor++) {
+            MotorOut[motor] = 1000;
+        }
+        updateMotors();     
 
         // Wait for all ESCs to acknowledge (2 + 1 beep)
         delay(4000);        
