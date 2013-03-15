c9cc157c876462a7e649077e1c55278829c8e5c1
cTn <ctndesigner@gmail.com>
2013-03-15 20:30:41 +0100

    manual accelerometer offset tuning support
    feature proposed by ReadError

--- extras/Configurator/tabs/initial_setup.js
+++ extras/Configurator/tabs/initial_setup.js
@@ -1,4 +1,10 @@
 function tab_initialize_initial_setup() {
+    // Fill in the manual calibration values from eeprom
+    var i = 0;
+    $('#content .calibrateAccelManual input').each(function() {
+        $(this).val(eepromConfig.ACCEL_BIAS[i++]);
+    });
+
     $('#content .calibrateESC').click(function() {
         eepromConfig.calibrateESC = parseInt(1);
         
@@ -47,6 +53,40 @@ function tab_initialize_initial_setup() {
         });   
     });
     
+    $('#content .calibrateAccelManualUpdate').click(function() {
+        // Update eeprom object
+        var i = 0;
+        $('.tab-initial_setup .calibrateAccelManual input').each(function() {
+            eepromConfig.ACCEL_BIAS[i++] = parseInt($(this).val());
+        });
+        
+        // Update flight controller with the latest data
+        var eepromConfigBytes = new ArrayBuffer(eepromConfigSize);
+        var view = new DataView(eepromConfigBytes, 0);
+        view.setUNION(eepromConfig); 
+
+        var bufferOut = new ArrayBuffer(5);
+        var bufView = new Uint8Array(bufferOut);
+        
+        // sync char 1, sync char 2, command, payload length MSB, payload length LSB, payload
+        bufView[0] = 0xB5; // sync char 1
+        bufView[1] = 0x62; // sync char 2
+        bufView[2] = 0x02; // command
+        bufView[3] = highByte(eepromConfigSize); // payload length MSB
+        bufView[4] = lowByte(eepromConfigSize); // payload length LSB   
+        
+        chrome.serial.write(connectionId, bufferOut, function(writeInfo) {});
+        
+        // payload
+        chrome.serial.write(connectionId, eepromConfigBytes, function(writeInfo) {
+            if (writeInfo.bytesWritten > 0) {
+                console.log("Wrote: " + writeInfo.bytesWritten + " bytes");
+                
+                command_log('Sending Configuration UNION to Flight Controller ...');
+            }    
+        });
+    });
+    
     $('#content .initializeEEPROM').click(function() {
         // initialize EEPROM
         var bufferOut = new ArrayBuffer(6);
@@ -82,4 +122,10 @@ function process_accel_calibration() {
     eepromConfig.ACCEL_BIAS[2] = data[2];
     
     console.log('Calibration received, data: ' + data);
+    
+    // Update the manual calibration inputs with latest data
+    var i = 0;
+    $('#content .calibrateAccelManual input').each(function() {
+        $(this).val(eepromConfig.ACCEL_BIAS[i++]);
+    });    
 }
