836cb5d4e12754cded4c57effecb848867acf4f1
cTn <ctndesigner@gmail.com>
2013-03-19 13:15:09 +0100

    YAW correction limited to max +-20%

--- libraries/P_FrameType/FrameType_QuadPlus.h
+++ libraries/P_FrameType/FrameType_QuadPlus.h
@@ -20,6 +20,9 @@
 uint16_t MotorOut[MOTORS] = {1000, 1000, 1000, 1000}; 
 
 void updateMotorsMix() {
+    // Limit YAW to +- 200 (20%)
+    YawMotorSpeed = constrain(YawMotorSpeed, -200, 200);
+    
     // All of the motor outputs are constrained to standard 1000 - 2000 us PWM
     // Minimum Armed Throttle variable was added to prevent motors stopping during flight (in extreme "stabilization" behaviours)
     MotorOut[0] = constrain(throttle + PitchMotorSpeed + YawMotorSpeed, CONFIG.data.minimumArmedThrottle, 2000);
