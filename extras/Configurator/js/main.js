e3a976165a1d1cf6f8ec8130d77118d03030218b
cTn <ctndesigner@gmail.com>
2013-04-20 14:19:56 +0200

    making room for AUX tab, also initializing files

--- extras/Configurator/js/main.js
+++ extras/Configurator/js/main.js
@@ -102,6 +102,8 @@ $(document).ready(function() {
                 $('#content').load("./tabs/pid_tuning.html", tab_initialize_pid_tuning);
             } else if ($(this).parent().hasClass('tab_channel_assigner')) {
                 $('#content').load("./tabs/channel_assigner.html", tab_initialize_channel_assigner);
+            } else if ($(this).parent().hasClass('tab_function_triggers')) {
+                $('#content').load("./tabs/function_triggers.html", tab_initialize_function_triggers);
             } else if ($(this).parent().hasClass('tab_sensor_data')) {
                 $('#content').load("./tabs/sensor_data.html", tab_initialize_sensor_data);
             } else if ($(this).parent().hasClass('tab_tx_rx')) {
---
+++ extras/Configurator/tabs/function_triggers.js
@@ -0,0 +1,2 @@
+function tab_initialize_function_triggers() {
+}
