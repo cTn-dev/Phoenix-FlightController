2b92240fa0da36afa12556c31ccfbcc56410ac77
cTn <ctndesigner@gmail.com>
2013-06-20 17:18:13 +0200

    merged m.command and m.output to a single tab

--- extras/Configurator/js/main.js
+++ extras/Configurator/js/main.js
@@ -140,8 +140,6 @@ $(document).ready(function() {
                 $('#content').load("./tabs/vehicle_view.html", tab_initialize_vehicle_view);
             } else if ($(this).parent().hasClass('tab_motor_output')) {
                 $('#content').load("./tabs/motor_output.html", tab_initialize_motor_output);
-            } else if ($(this).parent().hasClass('tab_motor_command')) {
-                $('#content').load("./tabs/motor_command.html", tab_initialize_motor_command);
             } else if ($(this).parent().hasClass('tab_gps')) {
                 $('#content').load("./tabs/gps.html", tab_initialize_gps);
             } else if ($(this).parent().hasClass('tab_about')) {
@@ -151,7 +149,7 @@ $(document).ready(function() {
     });
     
     // used for development only
-    //$('#content').load("./tabs/initial_setup.html", tab_initialize_initial_setup);
+    //$('#content').load("./tabs/motor_output.html", tab_initialize_motor_output);
 });
 
 function command_log(message) {
--- extras/Configurator/tabs/motor_output.js
+++ extras/Configurator/tabs/motor_output.js
@@ -1,18 +1,64 @@
 function tab_initialize_motor_output() {
+    // Request current state of motors
+    send_message(PSP.PSP_REQ_MOTORS_OUTPUT, 1); 
+    
+    // Enable specific titles
     for (var i = 0; i < motors; i++) {
         $('div.tab-motor_output .titles li:eq(' + i + ')').addClass('active');
     }
+
+    // Disable the rest
+    $('ul.sliders input').each(function() {
+        var index = $(this).parent().index();
+        
+        if (index >= motors) {
+            $(this).attr('disabled', 'disabled');
+        }
+    });
     
-    // request motor out data from flight controller
-    timers.push(setInterval(motor_pull, 50));
+    $('.tab-motor_output .stop').click(function() {
+        // reset all to 0
+        $('ul.sliders input').each(function() {
+            if ($(this).attr('disabled') != 'disabled') { // Protects .change event firing on motors that are disabled
+                $(this).val(0);
+                
+                // trigger change events
+                $(this).change();
+            }
+        });
+        
+    });    
+    
+    // Delayed initialize (due to current motor output requirements
+    setTimeout(function() {
+        for (var i = 0; i < motors; i++) {
+            $('ul.sliders input').eq(i).val((motors_output[i] - 1000) / 10);
+            $('ul.values li').eq(i).html(((motors_output[i] - 1000) / 10) + ' %');
+        }
+
+        // Change handler is "hooked up" after all the data is processed (to avoid double sending of the values)
+        $('ul.sliders input').change(function() {
+            var motor_n = parseInt($(this).parent().index()); // motor number
+            var motor_v = parseInt($(this).val()); // motor value
+            
+            // Update UI
+            $('ul.values li').eq(motor_n).html(motor_v + ' %');
+            
+            // Send data to flight controller
+            send_message(PSP.PSP_SET_MOTOR_TEST_VALUE, [motor_n, motor_v]);
+        }); 
+
+        // request motor out data from flight controller
+        timers.push(setInterval(motor_pull, 50));        
+    }, 25);    
 }
 
 function motor_pull() {
     // Render data
     for (var i = 0; i < motors; i++) {
         motors_output[i] -= 1000; 
-        var margin_top = 330.0 - (motors_output[i] * 0.33);
-        var height = (motors_output[i] * 0.33);
+        var margin_top = 130.0 - (motors_output[i] * 0.13);
+        var height = (motors_output[i] * 0.13);
         var color = parseInt(motors_output[i] * 0.256);
         $('.motor-' + i + ' .indicator').css({'margin-top' : margin_top + 'px', 'height' : height + 'px', 'background-color' : 'rgb(' + color + ',0,0)'});
     }
