57c8494c2e79ce5cae3fdf7a0b971811f0e425b3
cTn <ctndesigner@gmail.com>
2013-08-31 12:30:26 +0200

    optimizing

--- extras/Configurator/js/serial_backend.js
+++ extras/Configurator/js/serial_backend.js
@@ -37,7 +37,7 @@ var message_crc = 0;
 var char_counter = 0;
 
 function onCharRead(readInfo) {
-    if (readInfo && readInfo.bytesRead > 0 && readInfo.data) {
+    if (readInfo && readInfo.bytesRead > 0) {
         var data = new Uint8Array(readInfo.data);
         
         for (var i = 0; i < data.length; i++) {
